d9a54b5b59aeb21431becdd989a411dc55ae9343
omardoescode <omardoescode@gmail.com>
2024-12-15 07:03:03 +0200

    feat: Create App in presentation layer basic structure

--- presentation/include/App.hpp
+++ presentation/include/App.hpp
@@ -1,6 +1,38 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include "SFML/Window/Event.hpp"
 
 class App {
 public:
-  void fun() {}
+  App();
+  /*
+   * @brief Run the game
+   */
+  void run();
+
+private:
+  /*
+   * @brief Initliaze the window
+   */
+  void init_window();
+
+  /*
+   * @brief Handle all events of the game
+   */
+  void handle_events();
+
+  /*
+   * @brief Update the state of the game
+   */
+  void update();
+
+  /*
+   * @brief Render the game
+   */
+  void render();
+
+private:
+  std::unique_ptr<sf::RenderWindow> window;
+  sf::Event event;
 };
--- presentation/src/App.cpp
+++ presentation/src/App.cpp
@@ -1,1 +1,40 @@
 #include "App.hpp"
+#include <memory>
+#include "SFML/Window/Event.hpp"
+
+App::App() { init_window(); }
+
+void App::init_window() {
+  window = std::make_unique<sf::RenderWindow>(
+      sf::VideoMode::getFullscreenModes()[0], "SFML", sf::Style::Fullscreen);
+}
+
+void App::run() {
+  while (window->isOpen()) {
+    handle_events();
+    update();
+    render();
+  }
+}
+
+void App::handle_events() {
+  while (window->pollEvent(event)) {
+    if (event.type == sf::Event::Closed) {
+      window->close();
+    } else {
+      // TODO: handle elsewhere
+    }
+  }
+}
+
+void App::update() {
+  // update the game
+}
+
+void App::render() {
+  window->clear();
+
+  // render stuff
+
+  window->display();
+}
