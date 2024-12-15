4c213a872a31c08176ba4cee9c95063ae47574c4
omardoescode <omardoescode@gmail.com>
2024-12-15 07:03:39 +0200

    feat: Update the interface of the Map with `map_coords_to_tile`

--- business/include/Map/Map.hpp
+++ business/include/Map/Map.hpp
@@ -20,6 +20,16 @@ public:
   Map(std::vector<std::vector<std::shared_ptr<BaseTile>>>&&,
       std::list<std::shared_ptr<EnemyPathTile>>&& path);
 
+  /**
+   * @brief Map coordinates to a tile
+   *
+   * Given a coordinates in the game, return the tile on which these coordinates
+   * where
+   *
+   * @return Return the tile if it's in the game or nullptr instead
+   */
+  std::shared_ptr<BaseTile>& map_coords_to_tile(float x, float y) const;
+
 private:
   std::vector<std::vector<std::shared_ptr<BaseTile>>> grid;
   std::list<std::shared_ptr<EnemyPathTile>> enemyPath;
