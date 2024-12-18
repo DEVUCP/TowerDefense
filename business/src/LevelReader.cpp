661354511bed35f2c5af3aea6305ad093a309e57
moha09-coder <mohamedelmossalamy@gmail.com>
2024-12-18 23:12:37 +0200

    feat: implement all LevelReader methods

--- business/src/LevelReader.cpp
+++ business/src/LevelReader.cpp
@@ -3,9 +3,15 @@
 #include <Attack/AttackManager.hpp>
 #include <Enemy/EnemyManager.hpp>
 #include <Level.hpp>
+#include <Map/BaseTile.hpp>
+#include <Map/BuildableTile.hpp>
+#include <Map/EnemyPathTile.hpp>
+#include <Map/NonBuildableTile.hpp>
 #include <Tower/TowerManager.hpp>
 #include <WaveManager.hpp>
+#include <cassert>
 #include <filesystem>
+#include <fstream>
 
 /*
 File Format:
@@ -15,7 +21,6 @@ Line 3: Cockroach_Tickets Cockroach_StartingWave
 Line 4: Beetle_Tickets Beetle_StartingWave
 Line 5-22: "28 space-separated chatacters representing each tile type
 B=Buildable, N=NonBuildable, E=Enemy" e.g. "N B N E E N N N N B ......"
-#1#
 */
 // TODO: Make enemy initialization dynamic
 
@@ -30,11 +35,43 @@ std::shared_ptr<Level> LevelReader::build_level(int level_num) {
   int cockroach_tickets, cockroach_starting_wave;
   int beetle_tickets, beetle_starting_wave;
 
-  // TODO: Read the data from the respective level file e.g. Level 1 -> "1.txt",
-  // Level 2 -> "2.txt" First, read all the values initialized above
+  std::string path = get_level_path(level_num);
+  assert(std::filesystem::exists(path));
+
+  std::ifstream file(path);
+
+  // Read the first 4 lines of data in the level's file
+  file >> lives >> coins >> max_wave >> initial_enemy_count;
+  file >> ant_tickets >> ant_starting_wave;
+  file >> cockroach_tickets >> cockroach_starting_wave;
+  file >> beetle_tickets >> beetle_starting_wave;
 
   // Then, read the Map data
-  std::shared_ptr<Map> map;
+  std::vector<std::vector<std::shared_ptr<BaseTile>>> grid;
+  std::list<std::shared_ptr<EnemyPathTile>> enemy_path;
+
+  for (int i = 0; i < 18; i++) {
+    for (int j = 0; j < 28; j++) {
+      char c;
+      file >> c;
+
+      std::shared_ptr<BaseTile> tile;
+
+      if (c == 'B') {
+        tile = std::make_shared<BuildableTile>();
+      } else if (c == 'E') {
+        tile = std::make_shared<EnemyPathTile>();
+        enemy_path.push_back(std::static_pointer_cast<EnemyPathTile>(tile));
+      } else {
+        tile = std::make_shared<NonBuildableTile>();
+      }
+
+      grid[i].push_back(tile);
+    }
+  }
+
+  // Create the Map
+  auto map = std::make_shared<Map>(std::move(grid), std::move(enemy_path));
 
   // Create WaveManager with the initial enemy count and the total number of
   // waves
@@ -76,7 +113,19 @@ std::shared_ptr<Level> LevelReader::build_level(int level_num) {
 int LevelReader::levels_count() {
   int count = 0;
 
-  // std::filesystem::path data_dir("");
+  std::string levels_directory = get_level_path(-1);
+
+  for (auto& p : std::filesystem::directory_iterator(levels_directory)) {
+    count++;
+  }
 
   return count;
+}
+
+std::string LevelReader::get_level_path(int level_num = -1) {
+  if (level_num == -1)
+    return std::filesystem::current_path().string() + levels_path;
+
+  return std::filesystem::current_path().string() + levels_path +
+         std::to_string(level_num) + ".txt";
 }
