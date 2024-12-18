d57ac9402c1d07ea4f6fa771a54460ebf98fa489
omardoescode <omardoescode@gmail.com>
2024-12-18 21:42:07 +0200

    feat: update offsets and starting positons for the levels page

--- presentation/src/Pages/LevelsPage.cpp
+++ presentation/src/Pages/LevelsPage.cpp
@@ -12,8 +12,8 @@ LevelsPage::LevelsPage(unsigned w, unsigned h) : Page(w, h) {
   level_count = 8;  // hardcoded for now
 
   const int ROW_COUNT = 5;                 // 5 levels per row
-  const int startx = 300, starty = 200;    // Starting position
-  const int offsetx = 300, offsety = 200;  // Spacing between buttons
+  const int startx = 570, starty = 400;    // Starting position
+  const int offsetx = 200, offsety = 150;  // Spacing between buttons
   // Load the buttons
   for (int i = 0; i < level_count; i++) {
     int column = i % ROW_COUNT;  // Column index (0 to 4)
