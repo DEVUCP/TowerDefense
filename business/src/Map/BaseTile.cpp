886213ccc19c1dc6112a391e336ccd17a6a07bc6
DEVUCP <OMHN05@protonmail.com>
2024-12-19 21:23:13 +0200

    fix: changed return type in get_center() in BaseTile from int to unsigned

--- business/src/Map/BaseTile.cpp
+++ business/src/Map/BaseTile.cpp
@@ -6,6 +6,6 @@ BaseTile(float x, float y, TileType type)
     : position(Vector<float>(x, y)), type(type) {}
 
 Vector<float> get_center() const {
-  int tile_size = GameSettings::get_instance().get_tile_size();
+  unsigned tile_size = GameSettings::get_instance().get_tile_size();
   return Vector<float>(tile_size / 2, tile_size / 2)
 }
