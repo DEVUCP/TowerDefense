202de575d339dd44756c743756998a0145fbd576
DEVUCP <OMHN05@protonmail.com>
2024-12-19 19:59:17 +0200

    feat: Create source for Collidable coordinate getters

--- business/src/Utils/Collidable.cpp
+++ business/src/Utils/Collidable.cpp
@@ -11,3 +11,18 @@ Collidable::Collidable(float x, float y, float width, float height,
 void enable_collision() { this->enabled = true; }
 
 void disable_collision() { this->enabled = false; }
+
+private:
+Vector<float> tl() const { return position; }
+
+Vector<float> tr() const {
+  return Vector<float>(position.x + width, position.y);
+}
+
+Vector<float> bl() const {
+  return Vector<float>(position.x, position.y - height);
+}
+
+Vector<float> br() const {
+  return Vector<float>(position.x + width, position.y - height);
+}
