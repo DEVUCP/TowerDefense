8339dd124e6fb2e53f721d962060afc85ea02d5d
TheUCP <DEVUCP@protonmail.com>
2024-12-19 16:13:51 +0200

    fix: Add correct namespace for Drawable methods

--- business/src/Utils/Drawable.cpp
+++ business/src/Utils/Drawable.cpp
@@ -1,22 +1,15 @@
-Drawable::Drawable(const std::string& sprite, Vector<float> size, float rotation) {
+Drawable::Drawable(const std::string& sprite, Vector<float> size,
+                   float rotation) {
   this->size = size;
   this->sprite = sprite;
   this->rotation = rotation;
 }
 
-  const Vector<float>& Drawable::get_size() const{
-    return size;
-  }
+const Vector<float>& Drawable::get_size() const { return size; }
 
-  
-  const std::string& Drawable::get_sprite() const{
-    return sprite;
-  }
+const std::string& Drawable::get_sprite() const { return sprite; }
 
-  const float Drawable::get_rotation() const{
-    return rotation;
-  }
+const float Drawable::get_rotation() const { return rotation; }
 
-  void Drawable::set_rotation(float amount){
-    rotation = amount; 
-  }
+void Drawable::set_rotation(float amount) { rotation = amount; }
+when
