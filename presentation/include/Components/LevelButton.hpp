e01e7523ea3980c74a05d987270a08928398966b
omardoescode <omardoescode@gmail.com>
2024-12-19 08:06:54 +0200

    refactor: create a constant to easy control the font size for level button + increase font size

--- presentation/include/Components/LevelButton.hpp
+++ presentation/include/Components/LevelButton.hpp
@@ -5,6 +5,7 @@
 #include "SFML/Window/Window.hpp"
 
 #define LEVEL_NUM_COLOR 253, 226, 148
+#define LEVEL_NUM_FONT_SIZE 70
 class LevelButton : public IButton {
 public:
   LevelButton(unsigned x, unsigned y, int num);
