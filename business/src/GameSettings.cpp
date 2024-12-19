148ca40aec029fe329e82060dabdb1565c14a6cb
DEVUCP <OMHN05@protonmail.com>
2024-12-19 21:28:02 +0200

    feat: Create GameSettings source file

--- business/src/GameSettings.cpp
+++ business/src/GameSettings.cpp
@@ -1,5 +1,16 @@
+#include "GameSettings.hpp"
 #define WIDTH 1680
 #define HEIGHT 1080
 #define TILE_LENGTH 60
 #define ROW_NUM 18
 #define COLUMN_NUM 28
+
+GameSettings& GameSettings::get_instance() {
+  static GameSettings gs;
+  return gs;
+}
+
+Vector<unsigned> GameSettings::get_size() { return {WIDTH, HEIGHT}; }
+unsigned GameSettings::get_tile_size() { return TILE_LENGTH; }
+unsigned GameSettings::get_rows() { return ROW_NUM; }
+unsigned GameSettings::get_columns() { return COLUMN_NUM; }
