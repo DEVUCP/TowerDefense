25b36563e336d8ada4f1f576e4a841c12017ea8f
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 00:21:39 +0200

    feat: implement getter for enemy position

--- business/src/Enemy/BaseEnemy.cpp
+++ business/src/Enemy/BaseEnemy.cpp
@@ -13,6 +13,8 @@ void BaseEnemy::handle_next_tile_redirection(std::shared_ptr<Map> map) {}
 
 const float BaseEnemy::get_health() const { return health; }
 
+const std::shared_ptr<BaseTile> BaseEnemy::get_position() const { return current_tile; }
+
 const float BaseEnemy::get_initial_health() const { return initial_health; }
 
 const bool BaseEnemy::is_to_be_removed() const { return to_be_removed; }
