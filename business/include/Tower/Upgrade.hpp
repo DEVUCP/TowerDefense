87415bc17d2c087cd3c7d2298733c053c4153f85
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 00:23:33 +0200

    feat: add private variables for the Upgrade class

--- business/include/Tower/Upgrade.hpp
+++ business/include/Tower/Upgrade.hpp
@@ -5,4 +5,9 @@
 class Upgrade {
 public:
     Upgrade(std::string name, int cost, float range_increase, float damage_increase);
+private:
+    std::string name;        // Name of the upgrade
+    int cost;                // Cost to apply the upgrade
+    float range_increase;    // Increase in tower range
+    float damage_increase;   // Increase in tower damage
 };
