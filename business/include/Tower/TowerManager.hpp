71ae3e823df593fb5d101cfb81830e7782481d29
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 01:02:52 +0200

    feat: add implementation of adding a tower for the TowerManager class

--- business/include/Tower/TowerManager.hpp
+++ business/include/Tower/TowerManager.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
-class TowerManager {};
+#include <vector>
+#include <memory>
+#include "Tower/BaseTower.hpp"
+#include "Enemy/BaseEnemy.hpp"
+
+class TowerManager {
+public:
+    void add_tower(std::shared_ptr<BaseTower> tower);
+
+private:
+    std::vector<std::shared_ptr<BaseTower>> towers; // List of all towers managed by the TowerManager.
+};
