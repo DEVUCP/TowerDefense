6b8786c7313395dbd7b14d7021406f51129bc2c6
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 00:44:30 +0200

    docs: document Upgrade class and its constructor

--- business/include/Tower/Upgrade.hpp
+++ business/include/Tower/Upgrade.hpp
@@ -2,8 +2,22 @@
 
 #include <string>
 
+/**
+ * @brief Represents an upgrade for a tower in the game.
+ *
+ * Upgrades enhance a tower's capabilities, such as increasing its range
+ * or damage, at a specific cost.
+ */
 class Upgrade {
 public:
+    /**
+     * @brief Constructor to initialize an upgrade.
+     *
+     * @param name The name of the upgrade.
+     * @param cost The cost to apply the upgrade.
+     * @param range_increase The increase in the tower's range when the upgrade is applied.
+     * @param damage_increase The increase in the tower's damage when the upgrade is applied.
+     */
     Upgrade(std::string name, int cost, float range_increase, float damage_increase);
 
     std::string get_name() const;
