7c59ff6356fc1d789afd7adaefa28efbb29664dc
omardoescode <omardoescode@gmail.com>
2024-12-20 11:22:40 +0200

    fix: fix Collidable

--- business/src/Utils/Collidable.cpp
+++ business/src/Utils/Collidable.cpp
@@ -8,31 +8,30 @@ Collidable::Collidable(float x, float y, float width, float height,
 
 {}
 
-bool collide_with(const Collidable& other) {
+bool Collidable::collide_with(const Collidable& other) {
   return point_colliding(other.tl()) && point_colliding(other.tr()) &&
          point_colliding(other.bl()) && point_colliding(other.br());
 }
 
-bool point_colliding(Vector<float> point) {
+bool Collidable::point_colliding(Vector<float> point) {
   return (point.x >= position.x && point.x <= position.x + width) &&
-         (point.y >= position.y && point.y <= position.y - height)
+         (point.y >= position.y && point.y <= position.y - height);
 }
 
-void enable_collision() { this->enabled = true; }
+void Collidable::enable_collision() { this->enabled = true; }
 
-void disable_collision() { this->enabled = false; }
+void Collidable::disable_collision() { this->enabled = false; }
 
-private:
-Vector<float> tl() const { return position; }
+Vector<float> Collidable::tl() const { return position; }
 
-Vector<float> tr() const {
+Vector<float> Collidable::tr() const {
   return Vector<float>(position.x + width, position.y);
 }
 
-Vector<float> bl() const {
+Vector<float> Collidable::bl() const {
   return Vector<float>(position.x, position.y - height);
 }
 
-Vector<float> br() const {
+Vector<float> Collidable::br() const {
   return Vector<float>(position.x + width, position.y - height);
 }
