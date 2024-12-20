019aca605d0f643f05785c7cc438371ce6f6c63f
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 01:03:48 +0200

    feat: add implementation for upgrading and removing a tower

--- business/include/Tower/TowerManager.hpp
+++ business/include/Tower/TowerManager.hpp
@@ -9,6 +9,10 @@ class TowerManager {
 public:
     void add_tower(std::shared_ptr<BaseTower> tower);
 
+    void remove_tower(std::shared_ptr<BaseTower> tower);
+
+    void upgrade_tower(std::shared_ptr<BaseTower> tower, int upgrade_index);
+
 private:
     std::vector<std::shared_ptr<BaseTower>> towers; // List of all towers managed by the TowerManager.
 };
