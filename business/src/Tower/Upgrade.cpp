66f2046846027f5c53054b93ca60f04968f8477b
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 00:24:19 +0200

    feat: implement Upgrade constructor

---
+++ business/src/Tower/Upgrade.cpp
@@ -0,0 +1,5 @@
+#include "Tower/Upgrade.hpp"
+
+Upgrade::Upgrade(std::string name, int cost, float range_increase, float damage_increase)
+    : name(std::move(name)), cost(cost), range_increase(range_increase), damage_increase(damage_increase) {}
+
