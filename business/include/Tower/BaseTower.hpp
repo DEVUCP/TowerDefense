594b8a2af06d84b29f84b5552b50ff0d74c44fa8
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 00:29:07 +0200

    feat: add BaseTower constructor and private variables

--- business/include/Tower/BaseTower.hpp
+++ business/include/Tower/BaseTower.hpp
@@ -1,4 +1,19 @@
 #pragma once
 
+#include "Enemy/BaseEnemy.hpp"
+#include "Upgrade.hpp"
 #include "Utils/Drawable.hpp"
-class BaseTower : public Drawable {};
+
+class BaseTower : public Drawable {
+public:
+  BaseTower();
+
+private:
+  Vector<int> position;
+  float range;
+  float damage;
+  std::shared_ptr<Upgrade> offer_upgrades;
+  std::shared_ptr<Upgrade> upgrades;
+  std::string sprite;
+  std::string rotation;
+};
