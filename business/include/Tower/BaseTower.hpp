23a1c9f434ef39e5ff341a2cea80324244162c11
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 00:42:17 +0200

    docs: document BaseTower and its constructor

--- business/include/Tower/BaseTower.hpp
+++ business/include/Tower/BaseTower.hpp
@@ -4,8 +4,19 @@
 #include "Upgrade.hpp"
 #include "Utils/Drawable.hpp"
 
+/**
+ * @brief Represents the base functionality of a tower in the game.
+ *
+ * Towers attack enemies, have a specific range, can be upgraded, 
+ * and can be sold for a price based on their current upgrades.
+ */
 class BaseTower : public Drawable {
 public:
+  /**
+   * @brief Constructor
+   *
+   * Initializes all member variables and sets up the base properties of the tower.
+   */
   BaseTower();
 
   void init_attack(std::shared_ptr<BaseEnemy> enemy);
@@ -24,4 +35,8 @@ private:
   std::shared_ptr<Upgrade> upgrades;
   std::string sprite;
   std::string rotation;
+  Vector<int> position;           // The position of the tower on the map.
+  float range;                    // The attack range of the tower.
+  float damage;                   // The attack damage of the tower.
+  std::string sprite;             // The visual representation of the tower.
 };
