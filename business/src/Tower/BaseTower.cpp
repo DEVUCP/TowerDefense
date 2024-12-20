e892695e72105c95a7e5607de078c54b833da4c5
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 00:30:17 +0200

    feat: implement BaseTower constructor initialization for all variables

---
+++ business/src/Tower/BaseTower.cpp
@@ -0,0 +1,13 @@
+#include "Tower/BaseTower.hpp"
+#include "Tower/Upgrade.hpp"
+#include <iostream>
+
+#define BASE_DAMAGE 1.0f
+#define BASE_RANGE 1.0f
+
+BaseTower::BaseTower()
+    : Drawable(sprite, Vector<float>(0, 0)), position({0, 0}), range(BASE_RANGE), damage(BASE_DAMAGE), sprite(""), rotation("") {
+    // Initialize upgrades
+    offer_upgrades = std::make_shared<Upgrade>("Default Upgrade", 100, 10.0f, 5.0f);
+    upgrades = nullptr;
+}
