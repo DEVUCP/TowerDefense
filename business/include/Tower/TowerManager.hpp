6e85853cbcbd958ad006826b69aaf2b6b04be5ce
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 01:05:20 +0200

    feat: add implementation of a function that updates all towers

--- business/include/Tower/TowerManager.hpp
+++ business/include/Tower/TowerManager.hpp
@@ -11,6 +11,8 @@ public:
 
     void remove_tower(std::shared_ptr<BaseTower> tower);
 
+    void update(std::vector<std::shared_ptr<BaseEnemy>>& enemies);
+
     void upgrade_tower(std::shared_ptr<BaseTower> tower, int upgrade_index);
 
     int sell_tower(std::shared_ptr<BaseTower> tower);
