9fab927c9f667c17eaad6767e7dbfe62357d0dc9
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 01:15:31 +0200

    feat: implement the selling of a tower

--- business/src/Tower/TowerManager.cpp
+++ business/src/Tower/TowerManager.cpp
@@ -1,5 +1,6 @@
 #include "Tower/TowerManager.hpp"
 #include <iostream>
+#include <algorithm>
 void TowerManager::add_tower(std::shared_ptr<BaseTower> tower) {
     towers.push_back(tower);
     // TODO: Implement adding a tower to the tile position
@@ -33,5 +34,18 @@ void TowerManager::upgrade_tower(std::shared_ptr<BaseTower> tower, int upgrade_i
         std::cout << "Tower upgraded!" << std::endl;
     } else {
         std::cout << "Tower not found for upgrade!" << std::endl;
+    }
+}
+
+int TowerManager::sell_tower(std::shared_ptr<BaseTower> tower) {
+    auto it = std::find(towers.begin(), towers.end(), tower);
+    if (it != towers.end()) {
+        int sell_price = tower->get_sell_price();
+        towers.erase(it);
+        std::cout << "Tower sold for: " << sell_price << std::endl;
+        return sell_price;
+    } else {
+        std::cout << "Tower not found for selling!" << std::endl;
+        return 0;
     }
 }
