3171730bf7a8bb690a19e643170ba4d4a6cdd7a4
Marwan Mostafa <marwanm.dev@gmail.com>
2024-12-21 01:13:23 +0200

    docs: add todo in TowerManager

--- business/src/Tower/TowerManager.cpp
+++ business/src/Tower/TowerManager.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 void TowerManager::add_tower(std::shared_ptr<BaseTower> tower) {
     towers.push_back(tower);
+    // TODO: Implement adding a tower to the tile position
+    // will need a getter for get_position() ?
     // Vector<float> position = tower->get_position();
 }
 
