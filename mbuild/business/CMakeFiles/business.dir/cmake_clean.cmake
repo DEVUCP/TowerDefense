cae5e135ee49ebc64d6a522f3c49f7c70225ee03
YoussefEMS <yoyo.bob1972@gmail.com>
2024-12-24 11:11:16 +0200

    Add changes to map.cpp

---
+++ mbuild/business/CMakeFiles/business.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/business.dir/src/Game.cpp.obj"
+  "CMakeFiles/business.dir/src/Game.cpp.obj.d"
+  "libbusiness.dll"
+  "libbusiness.dll.a"
+  "libbusiness.dll.manifest"
+  "libbusiness.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/business.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mbuild/presentation/CMakeFiles/presentation.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/presentation.dir/src/App.cpp.obj"
+  "CMakeFiles/presentation.dir/src/App.cpp.obj.d"
+  "CMakeFiles/presentation.dir/src/Utils/TextBuilder.cpp.obj"
+  "CMakeFiles/presentation.dir/src/Utils/TextBuilder.cpp.obj.d"
+  "libpresentation.dll"
+  "libpresentation.dll.a"
+  "libpresentation.dll.manifest"
+  "libpresentation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/presentation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
