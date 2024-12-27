f37626f157c0c2b08f871e813349b1ae245d8495
DEVUCP <OMHN05@protonmail.com>
2024-12-27 23:10:49 +0200

    chore: Add missing BaseAttack methods to source file

--- business/src/Attack/BaseAttack.cpp
+++ business/src/Attack/BaseAttack.cpp
@@ -1,4 +1,5 @@
 #include "Attack/BaseAttack.hpp"
+#include <algorithm>
 
 BaseAttack::BaseAttack(float x, float y, float width, float height,
                        float velocity, Vector<float> target,
@@ -8,6 +9,14 @@ BaseAttack::BaseAttack(float x, float y, float width, float height,
       damage(damage) {}
 
 bool BaseAttack::is_to_be_removed() { return to_be_removed; }
+
+void BaseAttack::check_collisions(std::shared_ptr<Map> map) {}
+
+std::vector<std::shared_ptr<BaseTile>> BaseEnemy::filter_tiles(
+    std::vector<std::shared_ptr<BaseTile>> nearby) {}
+
+std::vector<std::shared_ptr<BaseTile>> BaseEnemy::get_nearby_tiles(
+    std::shared_ptr<Map> map) {}
 
 bool BaseAttack::hit(std::shared_ptr<BaseEnemy> enemy) {}
 
