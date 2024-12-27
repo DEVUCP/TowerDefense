cc4d4f9d029e316fe1b114cecba2e6a34d77c6a2
DEVUCP <OMHN05@protonmail.com>
2024-12-27 23:15:55 +0200

    feat: Create get_nearby_tiles BaseAttack method

--- business/src/Attack/BaseAttack.cpp
+++ business/src/Attack/BaseAttack.cpp
@@ -16,7 +16,15 @@ std::vector<std::shared_ptr<BaseTile>> BaseAttack::filter_tiles(
     std::vector<std::shared_ptr<BaseTile>> nearby) {}
 
 std::vector<std::shared_ptr<BaseTile>> BaseAttack::get_nearby_tiles(
-    std::shared_ptr<Map> map) {}
+    std::shared_ptr<Map> map) {
+  std::vector<std::shared_ptr<BaseTile>> nearby_tiles =
+      std::vector<std::shared_ptr<BaseTile>>();
+  nearby_tiles.push_back(map->map_coords_to_tile(tl().x, tl().y));
+  nearby_tiles.push_back(map->map_coords_to_tile(tr().x, tr().y));
+  nearby_tiles.push_back(map->map_coords_to_tile(bl().x, bl().y));
+  nearby_tiles.push_back(map->map_coords_to_tile(br().x, br().y));
+  return nearby_tiles;
+}
 
 bool BaseAttack::hit(std::shared_ptr<BaseEnemy> enemy) {}
 
