e339def4a71e5694ff91784a01b94e25d440d50d
DEVUCP <OMHN05@protonmail.com>
2024-12-28 03:38:24 +0200

    fix: MagmaCrab Constructor to include Collidable parameters

--- business/src/Enemy/Enemies/MagmaCrab.cpp
+++ business/src/Enemy/Enemies/MagmaCrab.cpp
@@ -1,8 +1,8 @@
 #include "Enemy/Enemies/MagmaCrab.hpp"
 #include "Enemy/BaseEnemy.hpp"
 
-MagmaCrab::MagmaCrab(float x, float y, Vector<float> dest)
-    : BaseEnemy(x, y, dest, INITIAL_HEALTH, VELOCITY, KILL_COINS,
+MagmaCrab::MagmaCrab(float x, float y, float width, float height, Vector<float> dest)
+    : BaseEnemy(x, y, width, height, dest, INITIAL_HEALTH, VELOCITY, KILL_COINS,
                 EnemyType::MAGMA_CRAB) {}
 void MagmaCrab::on_reach() {}
 void MagmaCrab::on_out_of_board() {}
