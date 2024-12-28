bb45b1c7b2fc080416d7ccbeeaa449dbb400fa49
omardoescode <omardoescode@gmail.com>
2024-12-28 15:41:23 +0200

    feat: Add more methods for the vectors

--- business/include/Utils/Vector.hpp
+++ business/include/Utils/Vector.hpp
@@ -30,8 +30,8 @@ public:
    * @note Must check length is not equal to 0
    */
   Vector normalize() {
+    assert(has_direction());
     T len = length();
-    assert(fabs(len) >= EPS);
 
     return {x / len, y / len};
   }
@@ -39,12 +39,38 @@ public:
   /*
    * @brief Get the length of a vector
    */
-  T length() { return std::sqrt(x * x + y * y); }
+  float length() { return std::sqrt(x * x + y * y); }
 
+  /**
+   * @brief Return The distance between two vectors using vector arithmetic
+   */
   float get_distance_to(Vector<T> destination) {
     return std::sqrt(std::pow(destination.x - x, 2) +
                      std::pow(destination.y - y, 2));
   }
+
+  /**
+   * @brief Return true if two vectors are in the same direction
+   *
+   * @brief
+   * Make sure neither is the origin point
+   */
+  bool is_same_direction_as(Vector<T> other) {
+    assert(has_direction());
+    assert(other.has_direction());
+    return this->normalize() == other.normalize();
+  }
+
+  /**
+   * @brief Return true if a vector is the same as the origin point (same as has
+   * no direction)
+   */
+  bool is_origin() { return x == 0 && y == 0; }
+
+  /**
+   * @brief Return true if the vector has direction
+   */
+  bool has_direction() { return !is_origin(); };
 
 public:
   T x;
