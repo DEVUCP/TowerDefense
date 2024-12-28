7eaf56c3ac7dda2db8ee5b9cfdf2370c263dbb96
moha09-coder <mohamedelmossalamy@gmail.com>
2024-12-28 16:09:06 +0200

    feat: implement all lose page buttons

--- presentation/src/Pages/LosePage.cpp
+++ presentation/src/Pages/LosePage.cpp
@@ -1,37 +1,49 @@
 #include "Pages/LosePage.hpp"
 #include <memory>
+
 #include "Components/MusicPlayer.hpp"
 #include "Components/TextButton.hpp"
 #include "Enums/Event.hpp"
+#include "Game.hpp"
 #include "SFML/Graphics/RenderTarget.hpp"
 #include "Widgets/MuteButton.hpp"
 
 LosePage::LosePage(unsigned width, unsigned height)
-    : Page(width, height), title("You lost!!", width, height) {
-  // Initialize the go back button
-  go_back =
-      std::make_shared<IconButton>("./assets/buttons/GoBack.png",
-                                   "./assets/buttons/GoBack_Hover.png", 60, 60);
-  go_back->set_handler([this]() { notify_observers(Event::GO_BACK_SWITCH); });
-  mute_button = std::make_shared<MuteButton>(60, height - 60);
+    : Page(width, height), title("Game Over", width, height) {
+
+  unsigned start_x = width/2, start_y = height/2;
+  int offset_y = 20;
+  int buttons_spacing_x = 80;
+
+  restart_button =
+      std::make_shared<IconButton>("./assets/buttons/Restart.png",
+                                   "./assets/buttons/Restart_Hover.png", start_x - buttons_spacing_x, start_y + offset_y);
+  restart_button->set_handler([this]() {
+    Game::get_instance().init_level(Game::get_instance().get_level()->get_level_num());
+    notify_observers(Event::GAME_PAGE_SWITCH);
+  });
+  levels_button =
+      std::make_shared<IconButton>("./assets/buttons/Levels.png",
+                                   "./assets/buttons/Levels_Hover.png", start_x + buttons_spacing_x, start_y + offset_y);
+  levels_button->set_handler([this]() { notify_observers(Event::LEVEL_PAGE_SWITCH); });
 }
 
 void LosePage::on_pause() {}
 void LosePage::on_unpause() {
   notify_observers(Event::BG_SHADOW_SWITCH);
+  // TODO: Change to lose music
   MusicPlayer::get_instance().set_current_music(MusicPlayer::LEVEL_MUSIC);
-  mute_button->check_status();
 }
 
 void LosePage::handle_events(EventData evt) {
-  go_back->handle_events(evt);
-  mute_button->handle_events(evt);
+  restart_button->handle_events(evt);
+  levels_button->handle_events(evt);
 }
 
-void LosePage::update(UpdateData dat) { mute_button->check_status(); }
+void LosePage::update(UpdateData dat) { }
 
 void LosePage::render(RenderData ren) {
   title.render(ren);
-  go_back->render(ren);
-  mute_button->render(ren);
+  restart_button->render(ren);
+  levels_button->render(ren);
 }
