605531a538f659cc8bfd8b1040c25c74974b966e
moha09-coder <mohamedelmossalamy@gmail.com>
2024-12-29 14:31:14 +0200

    feat: add const prices to ArcheryTower and IronPrism (susceptible to change)

--- business/include/Tower/Towers/ElectroTower.hpp
+++ business/include/Tower/Towers/ElectroTower.hpp
@@ -4,6 +4,7 @@
 class ElectroTower : public BaseTower {
 public:
   static constexpr unsigned RANGE = 1;
+  static constexpr unsigned PRICE = 50;
 
 public:
   ElectroTower(std::shared_ptr<BaseTile> tile);
---
+++ business/src/Tower/Towers/IonPrism.cpp
@@ -0,0 +1,5 @@
+#include "Tower/Towers/IonPrism.hpp"
+#include "Tower/BaseTower.hpp"
+
+IonPrism::IonPrism(std::shared_ptr<BaseTile> tile)
+    : BaseTower(tile, RANGE, BaseTower::IonPrism, PRICE) {}
