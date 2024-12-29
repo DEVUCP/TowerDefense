fdba4b99c3617d2450498d442173ac2e83229941
moha09-coder <mohamedelmossalamy@gmail.com>
2024-12-29 15:08:04 +0200

    feat: update BuildableTileView build_tower method to return true/false based on the successfulness of the purchase

--- presentation/src/Views/BuildableTileView.cpp
+++ presentation/src/Views/BuildableTileView.cpp
@@ -33,8 +33,13 @@ void BuildableTileView::update(UpdateData upd) {
 bool BuildableTileView::build_tower(BaseTower::TowerType type) {
   auto lvl = Game::get_instance().get_level();
   auto converted_tile = std::dynamic_pointer_cast<BuildableTile>(tile);
-  auto twr = lvl->build_tower(BaseTower::IonPrism, converted_tile);
-  tower = std::make_shared<TowerView>(twr);
-  std::cout << "made a tower" << std::endl;
-  return true;
+  auto twr = lvl->build_tower(type, converted_tile);
+  if(twr) {
+    tower = std::make_shared<TowerView>(twr);
+    std::cout << "made a tower" << std::endl;
+    return true;
+  } else {
+    std::cout << "you don't have enough coins to buy that tower" << std::endl;
+    return false;
+  }
 }
