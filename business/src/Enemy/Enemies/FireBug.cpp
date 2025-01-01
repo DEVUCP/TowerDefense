1110264f6b63a3c8511f998bbb92fba6d4b3deea
moha09-coder <mohamedelmossalamy@gmail.com>
2025-01-01 19:42:28 +0200

    chore: remove fire bug enemy

--- business/src/Enemy/Enemies/FireBug.cpp
+++
@@ -1,8 +0,0 @@
-#include "Enemy/Enemies/FireBug.hpp"
-
-FireBug::FireBug(float x, float y, Vector<float> dest)
-    : BaseEnemy(x, y, dest, INITIAL_HEALTH, VELOCITY, KILL_COINS,
-                EnemyType::CLAMP_BEETLE),
-      Positionable(x, y) {}
-
-void FireBug::on_killed() {}
