0ee5b2f7230b340e847a4ea3f8943ee37e05caab
moha09-coder <mohamedelmossalamy@gmail.com>
2025-01-01 19:41:08 +0200

    feat: add scorpion enemy

--- business/include/Enemy/Enemies/Scorpion.hpp
+++ business/include/Enemy/Enemies/Scorpion.hpp
@@ -2,14 +2,14 @@
 
 #include "Enemy/BaseEnemy.hpp"
 #include "Utils/Vector.hpp"
-class FireBug : public BaseEnemy {
+class Scorpion : public BaseEnemy {
 public:
   static constexpr int VELOCITY = 4;
   static constexpr int KILL_COINS = 6;
   static constexpr int INITIAL_HEALTH = 12;
 
 public:
-  FireBug(float x, float y, Vector<float> dest);
+  Scorpion(float x, float y, Vector<float> dest);
 
 private:
   virtual void on_killed() override;
